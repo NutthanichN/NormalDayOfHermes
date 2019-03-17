71b0ed490c921ad44ac6e05e37754cfcc702b1a8
Nutthanich Narphromar <monaprom134@gmail.com>
2019-03-17 15:04:15 +0700

    Change block size and sprite scale, add walls to SpriteList

--- zeus_vs_typhon.py
+++ zeus_vs_typhon.py
@@ -3,7 +3,8 @@ from models_zeus_vs_typhon import World, Character
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-BLOCK_SIZE = 40
+BLOCK_SIZE = 20
+SPRITE_SCALE = 0.75
 
 
 class ModelSprite(arcade.Sprite):
@@ -11,6 +12,8 @@ class ModelSprite(arcade.Sprite):
         self.model = kwargs.pop('model', None)
 
         super().__init__(*args, **kwargs)
+
+        self.scale = kwargs.pop('scale', None)
 
     def sync_with_model(self):
         if self.model:
@@ -27,18 +30,33 @@ class MapDrawer:
         self.width = self.map.width
         self.height = self.map.height
 
-        self.wall_sprite = arcade.Sprite('images/block_40.PNG')
+        self.wall_sprite = arcade.Sprite('images/block_20.PNG')
+        self.wall_sprite_list = arcade.SpriteList()
 
-    def draw_sprite(self, sprite, r, c):
-        x, y = self.get_sprite_position(r, c)
-        sprite.set_position(x, y)
-        sprite.draw()
+    # def draw_sprite(self, sprite, r, c):
+    #     x, y = self.get_sprite_position(r, c)
+    #     sprite.set_position(x, y)
+    #     sprite.draw()
+    #
+    # def draw(self):
+    #     for r in range(self.height):
+    #         for c in range(self.width):
+    #             if self.map.has_wall_at(r, c):
+    #                 self.draw_sprite(self.wall_sprite, r, c)
 
     def draw(self):
+        self.set_wall_list()
+        self.wall_sprite_list.draw()
+
+    def set_wall_list(self):
         for r in range(self.height):
             for c in range(self.width):
                 if self.map.has_wall_at(r, c):
-                    self.draw_sprite(self.wall_sprite, r, c)
+                    x, y = self.get_sprite_position(r, c)
+                    wall_sprite = arcade.Sprite('images/block_20.PNG')
+                    wall_sprite.center_x = x
+                    wall_sprite.center_y = y
+                    self.wall_sprite_list.append(wall_sprite)
 
     def get_sprite_position(self, r, c):
         r = r - 1
@@ -66,7 +84,8 @@ class CaveWindow(arcade.Window):
         arcade.set_background_color(arcade.color.SADDLE_BROWN)
 
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
-        self.hermes_sprite = ModelSprite('images/Hermes/Hermes_right_w1.PNG', model=self.world.hermes)
+        self.hermes_sprite = ModelSprite('images/Hermes/Hermes_right_w1.PNG', model=self.world.hermes,
+                                         scale=SPRITE_SCALE)
         self.map_drawer = MapDrawer(self.world.map1_1)
 
     def update(self, delta):
@@ -76,6 +95,7 @@ class CaveWindow(arcade.Window):
         arcade.start_render()
 
         self.map_drawer.draw()
+        # self.map_drawer.draw()
         self.hermes_sprite.draw()
 
     def on_key_press(self, key, key_modifiers):
